410a37ce6694ebecc6e45c9e21852f23c50abc02
Hiiragi0831 <glazunovden@yandex.ru>
2024-03-12 18:42:16 +0300

    start

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import Game from "./components/game";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Game/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
