8b6a44bcfa6d1f336e5bf838b3d89a63897f5efd
hir01 <glazunovden@yandex.ru>
2024-03-14 11:45:13 +0300

    game

--- src/components/Attributes.tsx
+++ src/components/Attributes.tsx
@@ -1,16 +1,11 @@
+import {useState} from "react";
+
 const Attribute = (props: any) => {
-    let name = props.name;
-    let value = props.value;
-
-    const count = (value: number) => {
-      return value + 1;
-    }
-
     return (
         <div className="attribute">
             <div className="attribute__name">{props.name}</div>
             <div className="attribute__value">{props.value}</div>
-            <button className="attribute__btn">+</button>
+            <button className="attribute__btn" onClick={props.onClick}>+</button>
         </div>
     )
 }
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,119 +1,103 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../style/Game.scss';
 import {Attribute} from "./Attributes";
 
-class Game extends React.Component {
-    state: {
-        name: string,
-        heal: number;
-        freePoints: number;
-        vitality: number;
-        evasion: number;
-        energy: number;
-        attributes: any;
+export default function Game() {
+    const hero = {
+        name: 'Alex',
+        freePoints: 50,
+        heal: 3,
+        vitality: 3,
+        evasion: 10,
+        energy: 0,
+        attributes: [
+            {
+                name: "Сила",
+                value: 0,
+                skills: [
+                    {
+                        name: "Атака",
+                        value: 0,
+                    }
+                ]
+            },
+            {
+                name: "Ловкость",
+                value: 0,
+                skills: [
+                    {
+                        name: "Стелс",
+                        value: 0,
+                    },
+                    {
+                        name: "Стрельба из лука",
+                        value: 0,
+                    },
+                ]
+            },
+            {
+                name: "Интеллект",
+                value: 0,
+                skills: [
+                    {
+                        name: "Обучаемость",
+                        value: 0,
+                    },
+                    {
+                        name: "Выживание",
+                        value: 0,
+                    },
+                    {
+                        name: "Медицина",
+                        value: 0,
+                    },
+                ]
+            },
+            {
+                name: "Харизма",
+                value: 0,
+                skills: [
+                    {
+                        name: "Запугивание",
+                        value: 0,
+                    },
+                    {
+                        name: "Проницательность",
+                        value: 0,
+                    },
+                    {
+                        name: "Внешний вид",
+                        value: 0,
+                    },
+                    {
+                        name: "Манипулирование",
+                        value: 0,
+                    },
+                ]
+            },
+        ]
     };
-    constructor(props: {} | Readonly<{}>) {
-        super(props);
 
-        this.state = {
-            name: 'Alex',
-            freePoints: 50,
-            heal: 3,
-            vitality: 3,
-            evasion: 10,
-            energy: 0,
-            attributes: [
-                {
-                    name: "Сила",
-                    value: 0,
-                    skills: [
-                        {
-                            name: "Атака",
-                            value: 0,
-                        }
-                    ]
-                },
-                {
-                    name: "Ловкость",
-                    value: 0,
-                    skills: [
-                        {
-                            name: "Стелс",
-                            value: 0,
-                        },
-                        {
-                            name: "Стрельба из лука",
-                            value: 0,
-                        },
-                    ]
-                },
-                {
-                    name: "Интеллект",
-                    value: 0,
-                    skills: [
-                        {
-                            name: "Обучаемость",
-                            value: 0,
-                        },
-                        {
-                            name: "Выживание",
-                            value: 0,
-                        },
-                        {
-                            name: "Медицина",
-                            value: 0,
-                        },
-                    ]
-                },
-                {
-                    name: "Харизма",
-                    value: 0,
-                    skills: [
-                        {
-                            name: "Запугивание",
-                            value: 0,
-                        },
-                        {
-                            name: "Проницательность",
-                            value: 0,
-                        },
-                        {
-                            name: "Внешний вид",
-                            value: 0,
-                        },
-                        {
-                            name: "Манипулирование",
-                            value: 0,
-                        },
-                    ]
-                },
-            ]
-        };
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
     }
 
-    attributeClick() {
-        const hero = Object.assign({}, this.state);
-
-    }
-
-    render() {
-        return (
-            <div className="Game">
-                <div className="Game__row">
-                    <h1>Game</h1>
-                    <div className="Game__person">
-                        <div className="Game__avatar">Avatar</div>
-                        <div className="Game__name"><input name="name" value={this.state.name}/></div>
-                    </div>
-                    <div className="Game__attributes">
-                        <Attribute name="Жизненная сила" value="0"></Attribute>
-                        <Attribute name="Уклонение" value="0"></Attribute>
-                        <Attribute name="Энергичность" value="0"></Attribute>
-                    </div>
+    return (
+        <div className="Game">
+            <div className="Game__row">
+                <h1>Game</h1>
+                <div className="Game__person">
+                    <div className="Game__avatar">Avatar</div>
+                    <div className="Game__name"><input name="name" value={hero.name}/></div>
+                </div>
+                <div className="Game__attributes">
+                    <Attribute name="Жизненная сила" value={count} onClick={handleClick}></Attribute>
+                    <Attribute name="Уклонение" value={count} onClick={handleClick}></Attribute>
+                    <Attribute name="Энергичность" value={count} onClick={handleClick}></Attribute>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default Game;
