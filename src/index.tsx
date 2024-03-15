cc95fbb50149fd852df71710d5a8d3e220751ba4
v.ostapov <vostapov@transportedlabs.com>
2024-03-15 18:41:41 +0300

    Bug: Init

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
+import './style/index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style/index.css';
-import reportWebVitals from './reportWebVitals';
-import Game from "./components/Game";
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
+import Game from './components/Game';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <React.StrictMode>
-        <Game/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
